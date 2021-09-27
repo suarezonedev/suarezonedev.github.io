de6b197ddfc02f35c71147984aeade5d3ff5bdc2
suarez <dikootimel384@gmail.com>
2021-09-27 11:15:00 +0300

    Create main.php

---
+++ main.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>why, rinki?</title>
+    <meta name="description" content="why... rinki...">
+    <link rel="shortcut icon" href="https://st.rinki.pw/assets/icon.rinki.jpg" type="image/x-icon">
+    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script> -->
+    <link href="https://fonts.googleapis.com/css2?family=Overpass:wght@100;200;300;400;600;700;800;900&display=swap" rel="stylesheet">
+    <style>*{margin:0;padding:0;color:#fff;font-family:Overpass,sans-serif;text-decoration:none}body{width:100%;height:100vh;background-color:#101010}#root{display:flex;justify-content:center;align-items:center;width:100%;height:100%}</style>
+</head>
+<body>
+    <div id="root">
+        <h2>whyrinki2</h2>
+    </div>
+     
+        
+<div class="preloader active">
+    <div class="preloader-items">
+        <img src="https://st.rinki.pw/rinw/avatarsq.png " alt="icon.layout" class="preloader-items-icon">
+        <div class="loading-bar"></div>
+    </div>
+</div>
+<script delete-after-load>
+    document.querySelector('[delete-after-load]').outerHTML='';
+    function addStyle(s){var t=document.createElement('style');t.innerHTML=s;document.body.appendChild(t);};
+    window.onload=()=>{
+        setTimeout(() => {
+            document.querySelector('.preloader').classList.remove('active') 
+            console.log(document.readyState);
+        }, 500);
+    }
+    addStyle('.preloader{position:fixed;z-index:9999;top:0;left:0;width:100%;height:100vh;background:#101010;display:flex;align-items:center;justify-content:center;text-align:center;transition:opacity 1.2s linear}.preloader:not(.active){pointer-events:none;opacity:0}.preloader-items{display:flex;align-items:center;justify-content:center;flex-direction:column;padding:30px 30px;border-radius:4px}.preloader-items-icon{width:72px;height:72px;border-radius:6px;margin-bottom:8px}.loading-bar{width:100px;height:4px;border-radius:6px;background:#303030;position:relative;overflow:hidden}.loading-bar::before{content:"";width:50%;height:4px;border-radius:6px;background:red;position:absolute;left:-34px;animation:loading-bar-animation 1.5s infinite ease}@keyframes loading-bar-animation{50%{left:96px}}')
+</script>  
+      
+</body>
+</html>
